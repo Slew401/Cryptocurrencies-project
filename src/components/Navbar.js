b77ea2e0fb7c639400fd5cc65eae9ef368e7e04c
Slew401 <nv20676@bristol.ac.uk>
2021-09-30 23:45:25 +0100

    Basic Menu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React from 'react'; 
 import { Avatar, Typography, Button, Menu} from "antd"; 
 import { Link } from 'react-router-dom';
-import { HomeOutlined, MoneyCollectFilled, BulbOutlined, FundOutlined } from "@ant-design/icons";
+import { HomeOutlined, MoneyCollectFilled, BulbOutlined, FundOutlined, MoneyCollectOutlined } from "@ant-design/icons";
 
 const icon = require('../images/crypto.png')
 
@@ -13,11 +13,21 @@ const Navbar = () => {
                <Typography.Title level = {2} className = "logo">
                    <Link to = "/">CryptoChecker</Link>
                </Typography.Title>
-               {/*<Button className = "menu-control-container">
-
-               </Button> */}
             </div>
-            
+            <Menu theme = "Dark">
+                <Menu.Item icon={<HomeOutlined/>}>
+                    <Link to = "/">Home</Link>
+                </Menu.Item>
+                <Menu.Item icon = {<FundOutlined/>}>
+                    <Link to = "/cryptocurrencies">Cryptocurrencies</Link>
+                </Menu.Item>
+                <Menu.Item icon = {<MoneyCollectOutlined/>}>
+                    <Link to = "/exchanges">Exchanges</Link>
+                </Menu.Item>
+                <Menu.Item icon = {<BulbOutlined/>}>
+                    <Link to = "/news">News</Link>
+                </Menu.Item>
+            </Menu>
         </div>
     )
 }
