085b2755f349eca085a154d834f91300e8f465f4
Slew401 <nv20676@bristol.ac.uk>
2021-10-04 21:23:50 +0100

    Redux Store Initialised

---
+++ src/services/cryptoAPI.js
@@ -0,0 +1,13 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit'; 
+
+/* var options = {
+  method: 'GET',
+  url: 'https://coinranking1.p.rapidapi.com/stats',
+  headers: {
+    'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+    'x-rapidapi-key': 'SIGN-UP-FOR-KEY'
+  }
+};
+
+*/
+
