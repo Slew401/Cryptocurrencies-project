9dc8f7b5ad88fc2a5bd4c8c0f8200da384c8478f
Slew401 <nv20676@bristol.ac.uk>
2021-10-05 23:22:05 +0100

    Homepage P2 Complete

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -3,18 +3,23 @@ import millify from 'millify'
 import { Typography, Row, Col, Statistic } from 'antd'
 import { Link } from 'react-router-dom'; 
 
+import { useGetCryptosQuery } from '../services/cryptoAPI';
+
 const { Title } = Typography; 
 
 const Homepage = () => {
+    const { data, isFetching } = useGetCryptosQuery(); 
+    const globalStats = data?.data?.stats;
+    console.log(globalStats);
     return (
         <div>
             <Title level = {2} className = "heading"> Global Crypto Stats</Title>
             <Row>
-                <Col span={12}><Statistic title = "Total CryptoCurrencies" value = "1000"/></Col>
-                <Col span={12}><Statistic title = "Total Exchanges" value = " 100"/></Col>
-                <Col span={12}><Statistic title = "Total Market Capitalisation" value = "1.5 Trillion"/></Col>
-                <Col span={12}><Statistic title = "Total 24-hour Volume" value = "100 Billion"/></Col>
-                <Col span={12}><Statistic title = "Total Markets" value = "China Sucks"/></Col>
+                <Col span={12}><Statistic title = "Total CryptoCurrencies" value = {globalStats.total}/></Col>
+                <Col span={12}><Statistic title = "Total Exchanges"  value = {globalStats.totalExchanges}/></Col>
+                <Col span={12}><Statistic title = "Total Market Capitalisation" value = {globalStats.totalMarketCap}/></Col>
+                <Col span={12}><Statistic title = "Total 24-hour Volume" value = {globalStats.total24hVolume}/></Col>
+                <Col span={12}><Statistic title = "Total Markets" value = {globalStats.totalMarkets}/></Col>
             </Row>
         </div>
     )
